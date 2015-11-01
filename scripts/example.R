d48da8823f2a2a7411c2b4d82e1b4f17d127b3b6
Aimee Barciauskas <abarciauskas@mdsol.com>
2015-11-01 19:24:03 +0100

    Modify example script

--- scripts/example.R
+++ scripts/example.R
@@ -1,11 +1,7 @@
 # install.packages(RMySQL)
 library(RMySQL)
 
-con <- dbConnect(RMySQL::MySQL(),
-  dbname = "gdelt",
-  host = "ds-group8.cgwo8rgbvpyh.eu-west-1.rds.amazonaws.com",
-  user = "group8",
-  password = Sys.getenv("DB_PASSWORD"))
+con <- dbConnect(RMySQL::MySQL(), user="root", password="root", dbname = "gdelt")
 
 res <- dbSendQuery(con, "select * from random_events")
 # n = -1 fetches all results, instead of default limit of first 500
