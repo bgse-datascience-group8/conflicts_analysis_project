d68ec85f043703308f2ba47365a1f861a8eb574f
Aimee Barciauskas <abarciauskas@mdsol.com>
2015-11-18 21:47:01 +0100

    Add updated script

--- scripts/shell/connect-db.sh
+++ scripts/shell/connect-db.sh
@@ -2,7 +2,7 @@
 mysql --host=group8-gdelt.cgwo8rgbvpyh.eu-west-1.rds.amazonaws.com --user=group8 --password=$DB_PASSWORD gdelt
 
 # Dump on remote
-mysqldump --host=group8-db.cgwo8rgbvpyh.eu-west-1.rds.amazonaws.com --user=group8 --password=$DB_PASSWORD gdelt usa_events_subset_random > usa_events_subset_random.sql
+mysqldump --host=group8-gdelt.cgwo8rgbvpyh.eu-west-1.rds.amazonaws.com --user=group8 --password=$DB_PASSWORD gdelt city_day_event_counts > city_day_event_counts.sql
 
 # Copy down dump file
 scp ubuntu@52.17.60.129:~/random_events_dump.sql .
