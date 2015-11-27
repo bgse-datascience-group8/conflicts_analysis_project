a3fc8822c11330664e80b653721962212d095bba
Aimee Barciauskas <abarciauskas@mdsol.com>
2015-11-27 17:02:25 +0100

    Add node app

---
+++ conflict_analysis_app/app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+app.get('/', function (req, res) {
+  res.render('index', {
+    title: 'Welcome'
+  });
+});
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'gdelt'
+});
+
+connection.connect();
+
+connection.query('select * from top_cities limit 1', function(err, rows, fields) {
+  if (err) throw err;
+  console.log('The solution is: ', rows[0]);
+});
+
+connection.end();
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
