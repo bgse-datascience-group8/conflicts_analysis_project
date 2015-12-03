1923165ba6f2b81b7bc91c27d646f51ef9fe74a6
Aimee Barciauskas <abarciauskas@mdsol.com>
2015-12-03 11:16:04 +0100

    Uncomment server code

--- conflict_analysis_app/app.js
+++ conflict_analysis_app/app.js
@@ -51,9 +51,9 @@ app.get('/about', function (req, res) {
 
 // connection.end();
 
-// var server = app.listen(3000, function () {
-//   var host = server.address().address;
-//   var port = server.address().port;
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
 
-//   console.log('Example app listening at http://%s:%s', host, port);
-// });
+  console.log('Example app listening at http://%s:%s', host, port);
+});
